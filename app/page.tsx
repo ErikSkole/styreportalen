864d05f02438ad330d83706098d770327e5f41f7
ErikSkole <e-erijoh6@iskule.no>
2024-01-10 09:44:50 +0100

    test

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-row items-center justify-end p-24">
       <div className="mainContent rounded-lg basis-1/2 ">
-        <div className=""></div>
+        <div className="">test</div>
       </div>
       <div className="filterBox rounded-lg basis-1/4">
         <div className=""></div>
