9fbda1bb0b0a6e97f3043c64a1cda07d2c604741
ErikSkole <e-erijoh6@iskule.no>
2024-01-23 23:27:02 +0100

    Got rid of "test" in main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="main flex min-h-screen flex-row items-center justify-center p-10">
       <div className="mainContent rounded-lg">
-        <div className="">test</div>
+        <div className=""></div>
       </div>
     </main>
   )
