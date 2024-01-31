d7d40501687dcc521864d49047c96dcb10b87d58
ErikSkole <e-erijoh6@iskule.no>
2024-01-31 09:38:54 +0100

    Add links to header and added filter components

--- app/components/search/filter.tsx
+++ app/components/search/filter.tsx
@@ -1,6 +1,38 @@
 export default function Filter() {
     return (
-        <main className="mainBox basis-1/4 h-1/2">
+        <main className="mainBox flex flex-col basis-1/4 h-1/2">
+            <h2 className='flex ml-6 mt-2'>Filter</h2>
+            <div className="searchBarFilter flex justify-center items-center">Søkefelt</div>
+            <div className="filter">
+                <div className="filterbox flex flex-col items-center mb-6">
+                    <div className="filterContent flex flex-row m-2">
+                        <img className="triangle" src="triangle.svg" />
+                        <h3>Filter navn</h3>
+                    </div>
+                    <div className="line"></div>
+                </div>
+                <div className="filterbox flex flex-col items-center mb-6">
+                    <div className="filterContent flex flex-row m-2">
+                        <img className="triangle" src="triangle.svg" />
+                        <h3>Filter navn</h3>
+                    </div>
+                    <div className="line"></div>
+                </div>
+                <div className="filterbox flex flex-col items-center mb-6">
+                    <div className="filterContent flex flex-row m-2">
+                        <img className="triangle" src="triangle.svg" />
+                        <h3>Filter navn</h3>
+                    </div>
+                    <div className="line"></div>
+                </div>
+                <div className="filterbox flex flex-col items-center mb-6">
+                    <div className="filterContent flex flex-row m-2">
+                        <img className="triangle" src="triangle.svg" />
+                        <h3>Filter navn</h3>
+                    </div>
+                    <div className="line"></div>
+                </div>
+            </div>
         </main>
     )
 }
--- app/components/search/search.tsx
+++ app/components/search/search.tsx
@@ -1,7 +1,7 @@
 export default function Search() {
     return (
         <main className="searchBox flex justify-center items-center w-screen h-16">
-            SearchBar
+            Søkefelt
         </main>
     )
 }
