b25caa47e7379fa3ecacb98222fce26c61324f65
ErikSkole <e-erijoh6@iskule.no>
2024-02-13 21:01:18 +0100

    changed back the color theme, and still working on infoPage

--- app/components/search/search.tsx
+++ app/components/search/search.tsx
@@ -1,7 +1,7 @@
 export default function Search() {
     return (
         <main className="searchBox flex justify-center items-center w-screen h-16">
-            Søkefelt
+            <h1 className="mt-8">Søk i arrangementer</h1>
         </main>
     )
 }
--- app/infoPage/page.tsx
+++ app/infoPage/page.tsx
@@ -4,14 +4,13 @@ export default function InfoPage() {
     return (
         <>
         <div className="infoPage flex flex-row justify-center">
-            <div className="mainBox infoPageBox basis-1/2">
-                <Image
-                className="infoPageImg"
-                src=""
-                alt=""
-                />
+            <div className="mainBox infoPageBox">
+                <img className="min-w-64 min-h-64 p-4" src="https://picsum.photos/200/300" alt="Bilde av arrangement" />
+                <div className="flex flex-col arrangementInfo p-4">
+                    <h1 className="arrangementTitle">Arrangement</h1>
+                    <p className="arrangementDescription">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima, ut quasi recusandae ea aspernatur ullam facilis eum assumenda fugiat similique eligendi, voluptatem amet vero doloremque reprehenderit! Rem nisi laudantium numquam.</p>
+                </div>
             </div>
-            <div className="mainBox basis-1/4"></div>
         </div>
         </>
     )
