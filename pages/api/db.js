e2ced9de0d47ee083a685c9fb5f20c94d21ff3dd
ErikSkole <e-erijoh6@iskule.no>
2024-02-14 08:59:25 +0100

    mysql2 is added

---
+++ pages/api/db.js
@@ -0,0 +1,21 @@
+// Import the mysql2 module
+import mysql from 'mysql2/promise';
+
+// Create a connection object with your database credentials
+const connection = await mysql.createConnection({
+  host: 'cpanel2.norplex.no',
+  user: 'portalwe_Amalie_Skram',
+  password: 'qTdWN^=#l%A6',
+  database: 'portalwe_program_public'
+});
+
+// Connect to the database
+console.log('Connected to the database!');
+
+// Perform queries and operations on the database
+const [results, fields] = await connection.query('SELECT * FROM users');
+console.log('The query results are: ', results);
+
+// Close the connection when done
+await connection.end();
+console.log('Disconnected from the database!');
