aa94ecf1449119bbd74518f34fe17cdba8677fbb
ErikSkole <e-erijoh6@iskule.no>
2024-02-21 09:36:53 +0100

    Does not work yet

---
+++ app/api/mysql/route.ts
@@ -0,0 +1,9 @@
+import mysql from 'mysql2/promise';
+
+let connectionParams = {
+    host: 'cpanel2.norplex.no',
+    port: 3306,
+    user: 'portalwe_Amalie_Skram',
+    password: 'qTdWN^=#l%A6',
+    database: 'portalwe_program_public'
+  }
--- app/infoPage/page.tsx
+++ app/infoPage/page.tsx
@@ -1,28 +1,5 @@
-import Image from "next/image"
-import { GetServerSideProps } from "next"
-import { connectToDatabase } from "../../db"
-
-
-export const getServerSideProps: GetServerSideProps = async () => {
-    const connection = await connectToDatabase();
-  
-    const [results] = await connection.query('SELECT * FROM public_events');
-    await connection.end();
-
-    return {
-      props: {
-        public_events: results
-      }
-    };
-  };
-  
-  const Page = ({  }) => {
-    return (
-      <div>
-        
-      </div>
-    );
-  };
-  
-  export default Page;
-
+export default function InfoPage() {
+  <>
+  <h1>Test</h1>
+  </>
+}
