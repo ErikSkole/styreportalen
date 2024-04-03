90a76c53cef4f32fcc7aef5e4a304a3b0f2f8682
PhillipLDaae1 <e-phidaa@iskule.no>
2024-04-03 10:21:42 +0200

    Changed name to title and description to name on front page

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -4,9 +4,6 @@ export default function Header() {
     return (
         <main className="header">
             <Link className='flex items-center' href="/"><h1>Program.no</h1></Link>
-            <button className='homeBtn'>
-                button
-            </button>
         </main>
     )
 }
