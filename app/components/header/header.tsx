ee16affcdb85716e815e07c52b1e15ccd7e1345b
PhillipLDaae1 <e-phidaa@iskule.no>
2024-04-03 10:39:47 +0200

    Added margin to program.no heading

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Header() {
     return (
         <main className="header">
-            <Link className='flex items-center' href="/"><h1>Program.no</h1></Link>
+            <Link className='flex items-center' href="/"><h1 className='ml-4'>Program.no</h1></Link>
         </main>
     )
 }
