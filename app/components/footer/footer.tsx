760df21327d57edd48d8c5e831e83d7a414a0bf8
PhillipLDaae1 <e-phidaa@iskule.no>
2024-04-03 09:54:18 +0200

    Fixed logo in footer

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -3,12 +3,15 @@ import Image from "next/image";
 export default function Footer() {
     return (
         <footer className="footer">
-            <Image
+            <div className="flex flex-col m-6 justify-center">
+                <Image className="rounded-lg"
                 src='/styreportalen.png'
                 alt="Styreportalen logo"
-                width={1000}
-                height={2}
-            />
+                width={500}
+                height={500}
+                 />
+            </div>
+            
         </footer>
     )
 }
