66468af48e102f2a21f72398ff0dbb50eb6c70d7
ErikSkole <e-erijoh6@iskule.no>
2024-05-15 08:28:52 +0200

    first

--- app/components/search/search.tsx
+++ app/components/search/search.tsx
@@ -1,7 +1,9 @@
 export default function Search() {
     return (
         <div className="searchBox flex justify-center items-center h-16">
-            <h1 className="mt-8">Søk i arrangementer</h1>
+            <h1 className="mt-8">
+                <input className="searchBar" type="text" id="searchBar" placeholder="Søk.."/>
+            </h1>
         </div>
     )
 }
